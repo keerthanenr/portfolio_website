38c5ace184222553b574bbf95f9f0052113325fd
keerthanenr <keerthanenr@live.co.uk>
2021-08-18 23:47:17 +0100

    Committing first files for portfolio website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          Keerthanen Ravichandran
+          <br></br>
+          I work as a software engineer at JPM Chase. 
+          <br></br>
+          I enjoy working on new technology and creating this website to demonstrate the technical skills and knowledge I have attained over my career. 
+          <br></br>
+          I have also added some of my interests and hobbies, feel free to get in touch via Linkedin or Instagram if you wish to discuss anything.
+    </div>
+  );
+}
+
+export default App;
